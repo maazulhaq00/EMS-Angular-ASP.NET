5af34cc0e494975b46ee7e355e6ccc408e36fe78
maaz <maazulhaq00@gmail.com>
2024-09-18 17:23:20 +0500

    http service and employee interface

---
+++ Frontend/src/app/interfaces/employee.ts
@@ -0,0 +1,8 @@
+export interface IEmployee {
+    employeeId: number,
+    employeeName: string,
+    employeeEmail: string,
+    employeePhone: string,
+    employeeAge: number,
+    employeeSalary: number
+}

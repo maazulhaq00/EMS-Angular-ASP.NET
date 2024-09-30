c456b67406b5745128e991319b83af3a3cfc58ea
maaz <maazulhaq00@gmail.com>
2024-09-30 17:53:57 +0500

    complete

--- Frontend1/src/app/interfaces/employee.ts
+++
@@ -1,8 +0,0 @@
-export interface IEmployee {
-    employeeId: number,
-    employeeName: string,
-    employeeEmail: string,
-    employeePhone: string,
-    employeeAge: string,
-    employeeSalary: string
-}
